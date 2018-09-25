d5a199639c5b3a2a537e05ce19b9e1b391cbdb02
renee.m.vieira@gmail.com <renee@github.com>
2018-09-25 17:31:26 +0200

    Made a genomics package. Added DNA and RNA functions.

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Genomics_demo_workshop
+
+This is a semo of a genomics python package 
+
+## Installation 
+```
+pip install 
+```
+
+## Usage 
+```python
+from genomics demo import DNA 
+dna_strand = DNA('AGTC') # Make DNA strand 
+dna_strand.complimentary_sequence # outputs "TCAG"
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='genomics demo',
+    version='v0.0.1',
+    packages=['genomics_demo'],
+    url='',
+    license='MPI',
+    author='Renee Vieira',
+    author_email='renee.m.vieira@gmail.com',
+    description='The best genomics package ever.  Just kidding--do NOT use this!'
+)
+
+
+
+
